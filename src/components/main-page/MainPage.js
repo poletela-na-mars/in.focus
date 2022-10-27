a6c1aa30036c3b89967b9bfcea67a6667977119e
Polina <polina-postylyakova@yandex.ru>
2022-10-28 01:36:13 +0300

    initial project settings, added MainPage component

---
+++ src/components/main-page/MainPage.js
@@ -0,0 +1,17 @@
+const MainPage = (props) => {
+    return (
+        <div className="main-page-block">
+            <a className="logo" href="#"></a>
+            <h1>Organize and plan your life with in.focus</h1>
+            <a href="#">Get started  →</a>
+        </div>
+    );
+};
+
+//TODO: -logo (svg/png) in a, img?
+//      -указать в манифесте
+//      -добавить в background анимацию
+//      -начальная марщрутизация
+//      -add normalize in main.css
+
+export default MainPage;
