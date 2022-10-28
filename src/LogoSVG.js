8fab2b50238073d477ffa831ba174fd67fdd35b4
Polina <polina-postylyakova@yandex.ru>
2022-10-28 22:45:48 +0300

    svg logo added, making main page, setting up sass

---
+++ src/LogoSVG.js
@@ -0,0 +1,11 @@
+import logo from "./in_focus_logo_dis.svg";
+
+const LogoSVG = (props) => {
+    return (
+        <div>
+            <img src={logo} alt={"Logo"} style={{height: props.height, width: props.width}}/>
+        </div>
+    );
+};
+
+export default LogoSVG;
--- src/components/main-page/MainPage.js
+++ src/components/main-page/MainPage.js
@@ -1,17 +1,22 @@
+import LogoSVG from "../../LogoSVG";
+import "./MainPage.scss";
+import {Link} from "react-router-dom";
+
 const MainPage = (props) => {
     return (
         <div className="main-page-block">
-            <a className="logo" href="#"></a>
-            <h1>Organize and plan your life with in.focus</h1>
-            <a href="#">Get started  →</a>
+            <Link to={"/"}><LogoSVG height={55} width={240}/></Link>
+            <p>Organize and plan your life with in.focus</p>
+            <Link to={"/"}>Get started  →</Link>
         </div>
     );
 };
 
-//TODO: -logo (svg/png) in a, img?
-//      -указать в манифесте
+//TODO:
 //      -добавить в background анимацию
 //      -начальная марщрутизация
 //      -add normalize in main.css
+//      -кнопки
+//      -отступы
 
 export default MainPage;
