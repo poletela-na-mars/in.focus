dcc86adbb50051470a2d20ca94af4203305955c0
Polina <polina-postylyakova@yandex.ru>
2022-11-01 00:02:50 +0300

    css animation on the main page

--- src/components/main-page/MainPage.js
+++ src/components/main-page/MainPage.js
@@ -4,25 +4,38 @@ import {Link} from "react-router-dom";
 
 const MainPage = (props) => {
     return (
-        <>
-            <div className="sign-in-button-container">
-                <Link to={"/"} className="sign-in-link">
-                    <button className="sign-in-button">Sign in</button>
-                </Link>
+        <div className="animation-area">
+            <div className="main-page-container">
+                <div className="sign-in-button-container">
+                    <Link to={"/"} className="sign-in-link">
+                        <button className="sign-in-button">Sign in</button>
+                    </Link>
+                </div>
+                <div className="main-page-block">
+                    <Link to={"/"} className="logo"><LogoSVG height={55} width={240}/></Link>
+                    <p className="main-text">Organize and plan your life with in.focus</p>
+                    <Link to={"/"} className="start-link">
+                        <button className="start-button">Get started →</button>
+                    </Link>
+                </div>
             </div>
-            <div className="main-page-block">
-                <Link to={"/"} className="logo"><LogoSVG height={55} width={240}/></Link>
-                <p className="main-text">Organize and plan your life with in.focus</p>
-                <Link to={"/"} className="start-link">
-                    <button className="start-button">Get started →</button>
-                </Link>
-            </div>
-        </>
+            <ul className="squares">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
     );
 };
 
 //TODO:
-//      -добавить в background анимацию
-//      -начальная марщрутизация
+//      -марщрутизация
 
 export default MainPage;
